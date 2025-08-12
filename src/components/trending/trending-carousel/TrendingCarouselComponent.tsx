2e6a4e4f5b8c3ef6052e24d741c1bb5bac4adfa2
Augusto Meireles do Nascimento <augustomeirelesn@hotmail.com>
2025-08-12 17:22:04 -0300

    feat: landscape breakpoint

--- src/components/trending/trending-carousel/TrendingCarouselComponent.tsx
+++ src/components/trending/trending-carousel/TrendingCarouselComponent.tsx
@@ -49,7 +49,7 @@ export default function TrendingCarouselComponent() {
           {games.map((game, index) => (
             <CarouselItem
               key={game.id}
-              className="basis-2/2 md:basis-1/3 lg:basis-1/5"
+              className="basis-2/2 carousel-item-landscape md:basis-1/3 lg:basis-1/5"
             >
               <div className="flex flex-col gap-4">
                 <Card className="relative h-[45vh] overflow-hidden">
