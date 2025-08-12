047cccfd96671fcfb3b206e0e73e92f7f29db034
Augusto Meireles do Nascimento <augustomeirelesn@hotmail.com>
2025-08-12 18:38:15 -0300

    feat: declaring links on footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,11 +2,11 @@ import Link from "next/link"
 
 
 const footerOptions = [
-    { label: 'Perguntas frequentes', href: '' },
-    { label: 'Entre em contato', href: '' },
-    { label: 'Central de ajuda', href: '' },
-    { label: 'Siga-me no Linkedin', href: '' },
-    { label: 'Siga-me no Github', href: '' },
+    { label: 'Perguntas frequentes', href: '/' },
+    { label: 'Entre em contato', href: '/' },
+    { label: 'Central de ajuda', href: '/' },
+    { label: 'Siga-me no Linkedin', href: 'https://www.linkedin.com/in/augusto-meireles-nascimento/' },
+    { label: 'Siga-me no Github', href: 'https://github.com/augustomdn' },
 
 ]
 
@@ -15,7 +15,7 @@ export default function Footer() {
         <footer className="p-8 bg-card border-t lg:px-40">
             <div className="py-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
                 {footerOptions.map((item, index) => (
-                    <Link href="/" key={index}>
+                    <Link href={item.href} key={index} target="_blank">
                         <span className="text-muted-foreground underline">
                             {item.label}
                         </span>
